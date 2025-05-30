b9b18777a1bd0c9e4a358477712b36c87dd3af95
Christopher Gantes <78065304+christophergantes@users.noreply.github.com>
2025-05-30 19:20:43 -0400

    added prettier config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./App.css";
-import {Link} from "react-router-dom"
+
+const useImageURL = () => {
+  const [imageURL, setImageURL] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => setImageURL(response[0].url))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { imageURL, error, loading };
+};
 
 const App = () => {
+  const { imageURL, error, loading } = useImageURL();
+
   return (
     <div>
       <h1>Hello from the main page of the app!</h1>
       <p>Here are some examples of links to other pages</p>
+      {loading ? (
+        <p>loading...</p>
+      ) : error ? (
+        <p>No picture is shown</p>
+      ) : (
+        imageURL && <img src={imageURL} alt="placeholder text" />
+      )}
       <nav>
         <ul>
           <li>
